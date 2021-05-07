44ed1cde7aecf5e44fb8190aebb15289b92d00a6
rohitkumar3success <rohit.kumar3@successive.tech>
2021-05-07 22:00:15 +0530

    Enum Task Implementation

---
+++ EnumTask.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EnumTask
+{
+    class Program
+    {
+        enum Months
+        {
+            January = 1,
+            February,
+            March,
+            April,
+            May,
+            June,
+            July,
+            August,
+            September,
+            October,
+            November,
+            December
+        };
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The {0}th month of the year is {1}", (int)Months.August, Months.August);
+        }
+    }
+}
