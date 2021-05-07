790f156890d4f7a5cb4ee6f6a248e3bf95917f39
rohitkumar3success <rohit.kumar3@successive.tech>
2021-05-07 11:46:39 +0530

    Oops Test (07/05/2021)

---
+++ Program (2).cs
@@ -0,0 +1,62 @@
+﻿using System;//Write a program for creating a Car in which Engine, Tyre, Sound System should be defined in a base class. Your program should inherit all the functionality and gives custom color to a car using interface
+
+namespace OopsTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car c = new Car();
+            c.CarType();
+            Console.ReadKey();
+
+        }
+    }
+
+    //creating a base class
+    class Tyre
+    {
+        protected void TyreType()
+
+        {
+            Console.WriteLine("Tubeless Tyre");
+
+        } }
+
+    class Sound
+    {
+        public void SoundType()
+        {
+            Console.WriteLine("Electric Music");
+
+        }
+
+
+    }
+
+    class color
+    {
+        protected void ColorType()
+        {
+            Console.WriteLine("Grey");
+        }
+    }
+
+
+    //creating child class
+    class Car : Tyre
+    {
+        public void CarType()
+        {
+            Console.WriteLine("Car Type: Mercdez");
+            CarType();
+        }
+        public void SoundType()
+        {
+            Console.WriteLine("SoundType:Electric");
+            SoundType();
+        }
+    }
+}
+
+  
